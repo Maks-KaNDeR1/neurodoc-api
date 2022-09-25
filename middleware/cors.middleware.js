346bf461a28ec4ca4e2e38907b02d9e90b19645b
Maks-KaNDeR <makskander1@gmail.com>
2022-09-25 15:04:41 +0300

    request

--- middleware/cors.middleware.js
+++ middleware/cors.middleware.js
@@ -1,7 +1,7 @@
 const cors = (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
+    req.header("Access-Control-Allow-Origin", "*");
+    req.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+    req.header("Access-Control-Allow-Headers", "Content-Type");
     next();
 }
 
